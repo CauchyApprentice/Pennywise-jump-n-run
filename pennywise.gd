35994931ffab831a5f9863c664bcb8048ee515d6
Raphael Sturm <schlangenkopf@gmx.de>
2025-08-31 20:02:37 +0200

    Added Balloon Hotkey

--- pennywise.gd
+++ pennywise.gd
@@ -3,6 +3,25 @@ extends CharacterBody2D
 
 const SPEED = 600
 const JUMP_VELOCITY = -800.0
+var balloon_scene = preload("res://balloon.tscn")
+
+var action_state: ActionState
+
+
+func shoot_balloon():
+	var ball: RigidBody2D = balloon_scene.instantiate()
+	get_parent().add_child(ball)
+	ball.position = position
+	ball.linear_velocity = Vector2(1000, 0)
+
+
+func _ready():
+	action_state = 
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("shoot"):
+		shoot_balloon()
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +30,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
